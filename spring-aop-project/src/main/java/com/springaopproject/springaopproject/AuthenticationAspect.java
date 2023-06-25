7a476564cb0d3a7ac56c226e9bfe4fd25a285f51
Eric Maniraguha <44385819+ericmaniraguha@users.noreply.github.com>
2023-06-25 02:56:40 +0200

    Add files via upload

---
+++ spring-aop-project/src/main/java/com/springaopproject/springaopproject/AuthenticationAspect.java
@@ -0,0 +1,25 @@
+package com.springaopproject.springaopproject;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class AuthenticationAspect {
+
+    @Pointcut("within(com..*)")
+    public void authenticatingPointCut(){
+
+    }
+    @Pointcut("within(com..* )")
+    public void authorizationPointCut(){
+
+    }
+    @Before("authenticatingPointCut() && authorizationPointCut()")
+    public void authenticate(){
+        System.out.println("Authenticate the request");
+    }
+    
+}
