1731a8638c10d5909264d655c06e020a3fab2349
Eric Maniraguha <44385819+ericmaniraguha@users.noreply.github.com>
2023-06-25 10:50:11 +0200

    Add files via upload
    Spring Boot Starters
    Creating REST APIs with Spring Boot
    Spring Boot DevTools

---
+++ springboot-tutorial/src/main/java/com/springbootcode/springboottutorial/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.springbootcode.springboottutorial.controller;
+
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    
+    // @RequestMapping(value = "/",method = RequestMethod.GET)
+    @GetMapping("/")
+    public String helloWorld(){
+        return "Welcome to daily code buffer!!!,...... ";
+    }
+}
